e81d16ea8404dcde1485b67dee120d2f3d4c10f1
Abdelrahman Badary <zetta_fci@hotmail.com>
2016-03-22 00:40:53 +0200

    digits added

--- ProjectX/app/src/main/java/com/projectx/graduation/projectx/MainActivity.java
+++ ProjectX/app/src/main/java/com/projectx/graduation/projectx/MainActivity.java
@@ -5,14 +5,34 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private AuthCallback authCallback ;
+
+    private static final String TWITTER_KEY = "VCRYx3WDx2z2CyTtwUTQBjRuD";
+    private static final String TWITTER_SECRET = "YChhMVQsrHzi5rFLFIndzGXE3cWY3IG05syxQ6v11mp0WdBew7";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +46,31 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits());
+
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
+
+    }
+
+    public AuthCallback getAuthCallback() {
+        return authCallback;
     }
 
     @Override
